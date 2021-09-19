69fd3d7a6ab7caa169b592bff32c171ba6ebd178
Turquoisekitty <caizz17@mails.tsinghua.edu.cn>
2021-09-19 14:20:04 +0800

    new model

--- model.py
+++ model.py
@@ -9,6 +9,43 @@ import torch
 def swish(x):
     return x * torch.sigmoid(x)
 
+
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+    def __init__(self,in_dim,activation):
+        super(Self_Attn,self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax  = nn.Softmax(dim=-1) #
+
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width ,height = x.size()
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N) 
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(m_batchsize,C,width,height)
+
+        out = self.gamma*out + x
+        return out#, attention
+
+
 class ResBlock(nn.Module):
     def __init__(self, args, downsample=True, rescale=True, filters=64, \
         norm=True, spec_norm=False, alias=False):
@@ -21,6 +58,10 @@ class ResBlock(nn.Module):
             self.bn1 = nn.InstanceNorm2d(filters, affine=True)
         else:
             self.bn1 = nn.GroupNorm(32, filters)
+
+        # self.bn1 = InstanceNorm(filters, affine=True)
+        # self.bn1 = nn.InstanceNorm2d(filters, affine=True)
+        # self.bn1 = nn.GroupNorm(32, filters, affine=True)
 
         if not norm:
             self.bn1 = None
@@ -38,6 +79,10 @@ class ResBlock(nn.Module):
         else:
             self.bn2 = nn.GroupNorm(32, filters, affine=True)
 
+        # self.bn2 = InstanceNorm(filters, affine=True)
+        # self.bn2 = nn.InstanceNorm2d(filters, affine=True)
+        # self.bn2 = nn.GroupNorm(32, filters, affine=True)
+
         if not norm:
             self.bn2 = None
 
@@ -50,8 +95,9 @@ class ResBlock(nn.Module):
         self.dropout = nn.Dropout(0.2)
 
         # self.relu = nn.ReLU(inplace=True)
-        self.relu = nn.ELU(inplace=True)
-        self.act = swish
+        # self.relu = nn.ELU(inplace=True)
+        # self.act = swish
+        self.act = nn.ELU(inplace=True)
 
         # Upscale to mask of image
         if downsample:
@@ -100,7 +146,8 @@ class ResBlock(nn.Module):
 class ResNetModel(nn.Module):
     def __init__(self, args, stride=2):
         super(ResNetModel, self).__init__()
-        self.act = swish
+        # self.act = swish
+        self.act = nn.ELU(inplace=True)
 
         self.args = args
         self.spec_norm = False
@@ -115,18 +162,18 @@ class ResNetModel(nn.Module):
         self.conv1 = nn.Conv2d(3, filter_dim, kernel_size=7, stride=stride, padding=3)
 
         self.res_1a = ResBlock(args, filters=filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
-        self.res_1b = ResBlock(args, filters=filter_dim, rescale=False, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_1b = ResBlock(args, filters=filter_dim, rescale=True, spec_norm=self.spec_norm, norm=self.norm)
 
-        self.res_2a = ResBlock(args, filters=filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
-        self.res_2b = ResBlock(args, filters=filter_dim, rescale=True, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_2a = ResBlock(args, filters=2*filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_2b = ResBlock(args, filters=2*filter_dim, rescale=True, spec_norm=self.spec_norm, norm=self.norm)
 
-        self.res_3a = ResBlock(args, filters=2*filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
-        self.res_3b = ResBlock(args, filters=2*filter_dim, rescale=True, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_3a = ResBlock(args, filters=4*filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_3b = ResBlock(args, filters=4*filter_dim, rescale=True, spec_norm=self.spec_norm, norm=self.norm)
 
-        self.res_4a = ResBlock(args, filters=4*filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
-        self.res_4b = ResBlock(args, filters=4*filter_dim, downsample=False, rescale=False, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_4a = ResBlock(args, filters=5*filter_dim, downsample=False, spec_norm=self.spec_norm, norm=self.norm)
+        self.res_4b = ResBlock(args, filters=5*filter_dim, downsample=False, rescale=False, spec_norm=self.spec_norm, norm=self.norm)
 
-        # self.self_attn = Self_Attn(2 * filter_dim, self.act)
+        self.self_attn = Self_Attn(4 * filter_dim, self.act)
 
         # self.energy_map = nn.Linear(filter_dim*8, 1)
 
@@ -142,7 +189,7 @@ class ResNetModel(nn.Module):
         x = self.res_2b(x)
 
         # if self.args.self_attn:
-        # x, _ = self.self_attn(x)
+        x = self.self_attn(x)
 
         x = self.res_3a(x)
         x = self.res_3b(x)
@@ -151,5 +198,3 @@ class ResNetModel(nn.Module):
         x = self.res_4b(x)
         # x = self.act(x)
         return x
-
-
